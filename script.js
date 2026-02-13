4a3e9926329c9c885031dffb89493b47b7b88dde
Ricardo Souza <58848748+relitosouza@users.noreply.github.com>
2026-02-12 21:37:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const rotationOrder = ['abertos', 'andamento', 'resolvidos'];
     let currentIndex = 0;
-    const rotationIntervalTime = 5000; // 5 seconds
+    const rotationIntervalTime = 15000; // 5 seconds
     let rotationInterval;
     let isRotationActive = true;
     
